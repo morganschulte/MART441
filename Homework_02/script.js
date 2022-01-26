fe62a355dd991c6ec021ca4a8a5be5ad091fb18b
Morgan Schulte <60452807+morganschulte@users.noreply.github.com>
2022-01-26 13:46:01 -0700

    update

--- Homework_02/script.js
+++ Homework_02/script.js
@@ -1,14 +1,3 @@
-/* For your homework this week, extend your first web page that uses the console.log and adds some comments.
-You are welcome to make something display on the page like an image or something.
-
-Execute the log from a separate JavaScript file that you link to from your HTML document.
-Use the script tag at the end of your HTML document to do this
-The console.log should include;
-3 of your favorite websites
-3 of your favorite games if you play any - they can be board games, etc.
-3 of your favorite artists and their cultural importance
-You should set a primary background color for your document using a style element within the HTML document’s head element. */
-
 console.log('My three favorite websites are:');
 console.log('https://wovenmagazine.com');
 console.log('https://www.onthesnow.com/montana/skireport');
@@ -28,4 +17,3 @@ console.log('Lee Nye. His single-light portraits line the walls of Charlie Bs in
 console.log('These portraits of patrons have been hanging for over 50 years, and have watched as Missoula shifts and changes with time.');
 console.log('My friends and I have played games to guess which patron we are imatating with our facial expressions.');
 console.log('Nyes work might not have importance outside of this town, but for Missoula, Lee Nyes work is the most well known body of artwork in town.');
-console.log('');
