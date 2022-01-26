800799afffe01f30be4dfe75aa83d6766f98aa41
Morgan Schulte <60452807+morganschulte@users.noreply.github.com>
2022-01-26 13:00:58 -0700

    Update script.js

---
+++ Homework_02/script.js
@@ -0,0 +1,24 @@
+/* For your homework this week, extend your first web page that uses the console.log and adds some comments.
+You are welcome to make something display on the page like an image or something.
+
+Execute the log from a separate JavaScript file that you link to from your HTML document.
+Use the script tag at the end of your HTML document to do this
+The console.log should include;
+3 of your favorite websites
+3 of your favorite games if you play any - they can be board games, etc.
+3 of your favorite artists and their cultural importance
+You should set a primary background color for your document using a style element within the HTML document’s head element. */
+
+console.log('My three favorite websites are:');
+console.log('https://wovenmagazine.com');
+console.log('https://www.onthesnow.com/montana/skireport');
+console.log('https://missoula.craigslist.org/');
+console.log('');
+console.log('');
+console.log('');
+console.log('');
+console.log('');
+console.log('');
+console.log('');
+console.log('');
+console.log('');
